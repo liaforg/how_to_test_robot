6218d34a644bc6fd8fda0994311e7cf78bc42b7f
Lia Forgione <liaforg@gmail.com>
2017-06-22 11:55:56 -0400

    all 9 tests

--- test_robot.rb
+++ test_robot.rb
@@ -4,76 +4,57 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
-  def test_that_foreign_robot_neeing_repairs_sent_to_station_1
-    skip
-    # arrange
+  def test_that_foreign_robot_needing_repairs_sent_to_station_1
 
-    # act
-
-    # assert
+    @robot = Robot.new
+    @robot.needs_repairs = true
+    @robot.foreign_model = true
+    assert @robot.station == 1
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    @robot.needs_repairs = true
+    @robot.vintage_model = true
+    assert @robot.station == 2
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    @robot.needs_repairs = true
+    send_to_station = @robot.station
+    assert_equal(send_to_station, 3)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    send_to_station = @robot.station
+    assert_equal(send_to_station, 4)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    to_do = @robot.prioritize_tasks
+    assert_equal(to_do, -1)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
-    # arrange
 
-    # act
-
-    # assert
+   @robot = Robot.new
+   @robot.todos = [2,3,1]
+   assert @robot.prioritize_tasks == 3
   end
 
   def test_workday_on_day_off_returns_true
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    @robot.day_off = "Monday"
+    refute @robot.workday?("Monday")
   end
 
   def test_workday_not_day_off_returns_false
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    @robot = Robot.new
+    @robot.day_off = "Monday"
+    refute @robot.workday?("Monday")
   end
 
 end
